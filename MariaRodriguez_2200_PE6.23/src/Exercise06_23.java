e1ee308357d436df4740ac1d79abd9ccf345c890
Student <Student@10.100.208.70>
2022-11-09 11:08:30 -0600

    PE6.23

---
+++ MariaRodriguez_2200_PE6.23/src/Exercise06_23.java
@@ -0,0 +1,38 @@
+/********************************************************************************************
+ * 
+ * @author Maria Rodriguez
+ * Date: 11/09/2022 [W]
+ *
+ *********************************************************************************************/
+import java.util.Scanner;
+public class Exercise06_23 {
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		System.out.print("Enter a string: ");
+		String s = input.nextLine();
+		
+		System.out.print("Enter a character: ");
+		char c = input.nextLine().charAt(0);
+		
+		int count = count(s,c);
+		System.out.println("The number of occurrences of " + c + " in " + s + " is " + count);
+		
+		input.close();
+		
+	}
+	
+
+		
+		public static int count(String str, char ch) {
+			int count = 0;
+			for(int i = 0; i < str.length(); i++ )
+				if(str.charAt(i) == ch ) {
+					count++;
+					
+				}
+
+			
+			return count;
+			
+		}
+	}
