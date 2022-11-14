fbec5664f2bc3920adc873eb06d84afd13b52d4e
Student <Student@10.100.216.84>
2022-11-14 10:34:25 -0600

    PE6.4

---
+++ MariaRodriguez_2200_PE6.4/src/Exercise06_04.java
@@ -0,0 +1,29 @@
+/*********************************************************************************************
+ * 
+ * @author Maria Rodriguez
+ * Date: 11/14/2022 [M]
+ *
+ *********************************************************************************************/
+import java.util.Scanner;
+public class Exercise06_04 {
+	public static void main(String[] args) {
+		System.out.print("Enter an integer: ");
+		Scanner input = new java.util.Scanner(System.in);
+		int number = input.nextInt();
+		reverse(number);
+		input.close();
+	}
+	
+	public static void reverse(int n) {
+		if( n == 0) {
+			System.out.print(n);
+		}
+		while( n !=0) {
+			int remainder = n % 10;
+			System.out.print(remainder);
+			n = n / 10;
+			
+		}
+	}
+
+}
